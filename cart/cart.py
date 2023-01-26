d0703faba26840833e88ddad75da4f1cfa0ba475
Karol <kurekhombre@gmail.com>
2023-01-27 00:11:51 +0100

    Issue #37, #38, add add_to_cart in urls, block title to templates, fix iterator and add get_total_cost method, fix start shopping button

--- cart/cart.py
+++ cart/cart.py
@@ -17,6 +17,11 @@ class Cart(object):
         for p in self.cart.keys():
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
 
+        for item in self.cart.values():
+            item['total_price'] = item['product'].price * item['quantity']
+
+            yield item
+
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
 
@@ -42,3 +47,7 @@ class Cart(object):
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def get_total_cost(self):
+        return sum(item['total_price'] for item in self.cart.values())
+
